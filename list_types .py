e0c996b6f7a468531036446b9b77ab229678e32f
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-06 14:49:21 +0200

    Add files via upload

---
+++ list_types .py
@@ -0,0 +1,16 @@
+# storing the names of three of my friends using lists 
+
+"step1 "
+# created a list to print the first name 
+friends_names = ["Jadon Sancho","Bruno Fernandes","Marcus Rashford"]
+print(friends_names[0])
+
+# prints out the last name 
+print(friends_names[-1])
+
+# used the len function to print out the number of characters inside list 
+friends_names = len(["Jadon Sancho","Bruno Fernandes","Marcus Rashford"])
+print(friends_names)
+
+# created a variable to store the ages of 3 friends using a list 
+friends_age = [22,27,25]
